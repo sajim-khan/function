a92dd116188828de10b4e9cb2155ff40179d6e7e
sajim-khan <shossain1427@gmail.com>
2023-02-05 19:01:31 +0600

    basic function

--- first.js
+++ first.js
@@ -1,1 +1,15 @@
-console.log("jhdf")
+//console.log("Function")
+// function declaration
+function startFan(){
+    console.log("stand up");
+    console.log("walk towards the switch");
+    console.log("press the switch");
+}
+startFan();
+
+//parameter declarations
+function bringSingara(money){
+    console.log("Mama singara den");
+    console.log(money);
+}
+bringSingara(100);
